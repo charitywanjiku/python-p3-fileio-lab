58e7859451775d887cd611e65a3d1520ce53bf9c
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-03-06 13:31:08 +0300

    Assignment done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,30 @@
-def write_file(file_name, file_content):
-    pass
+"""def write_file(file_name, file_content):
+    file_name+=".txt"
+    with open(file_name,"w")as file:
+        file.write(file_content)
+    
 
 def append_file(file_name, append_content):
-    pass
+    file_name+=".txt"
+    with open(file_name,"a")as file:
+     file.write(append_content)
+    
 
 def read_file(file_name):
-    pass
+    file_name+=".txt"
+    with open(file_name,"r")as file:
+       return file.read()"""
+def write_file(file_name, file_content):
+    with open(f"{file_name}.txt", mode='w', encoding='utf-8') as file:  
+     file.write(file_content)
+    
+
+def append_file(file_name, append_content):
+    with open(f"{file_name}.txt", encoding='utf-8', mode='a') as file:
+        file.write(append_content)
+
+def read_file(file_name):
+    with open(f"{file_name}.txt", encoding='utf-8', mode='r') as file:
+        return file.read()
+       
+    
